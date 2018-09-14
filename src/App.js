eea392b682aa0a4eac7b9edbe64db9c103497851
Prateek Baheti <prateekb@thoughtworks.com>
2018-09-14 17:43:26 +0530

    Making App a functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Clock from "./clock";
 import Products from "./products";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome to React</h1>
-        <Clock />
-        <Products />
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+      <Clock />
+      <Products />
+    </div>
+  );
+};
